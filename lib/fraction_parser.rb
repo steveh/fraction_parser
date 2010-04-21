1b340ab47e8cfcc6499fba19be214e54acc01171
Steve Hoeksema <steve@seven.net.nz>
2010-04-21 18:57:01 +1200

    more robust parser

--- lib/fraction_parser.rb
+++ lib/fraction_parser.rb
@@ -1,13 +1,34 @@
+require "rational"
+
 class FractionParser
 
   def self.parse(string)
     string = string.strip
     if string =~ /^((\d+)\s+)?((\d+)\/(\d+))$/
-      sum = Rational($4, $5)
+      sum = Rational($4.to_i, $5.to_i)
       sum += $2.to_i if $2
       sum
     else
-      sum = Rational(string.to_f, 1)
+      sum = self.float_to_rational(string.to_f)
+    end
+  end
+
+  # Whoa this method is crazy
+  # I nicked it from Jannis Harder at http://markmail.org/message/nqgrsmaixwbrvsno
+  def self.float_to_rational(value)
+    if value.nan?
+      return Rational(0,0) # Div by zero error
+    elsif value.infinite?
+      return Rational(value<0 ? -1 : 1,0) # Div by zero error
+    end
+
+    s,e,f = [value].pack("G").unpack("B*").first.unpack("AA11A52")
+    s = (-1)**s.to_i
+    e = e.to_i(2)
+    if e.nonzero? and e<2047
+      Rational(s)* Rational(2)**(e-1023)*Rational("1#{f}".to_i(2),0x10000000000000)
+    elsif e.zero?
+      Rational(s)* Rational(2)**(-1024)*Rational("0#{f}".to_i(2),0x10000000000000)
     end
   end
 
--- spec/fraction_parser_spec.rb
+++ spec/fraction_parser_spec.rb
@@ -2,20 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "FractionParser" do
 
-  def should_parse(string, float)
-    FractionParser.parse(string).to_f.should eql(float)
+  def should_parse_fraction(string, float)
+    fp = FractionParser.parse(string)
+    fp.to_f.should eql(float)
   end
 
   it "should parse fractions" do
-    should_parse("3/4", 0.75)
-    should_parse("2 1/16", 2.0625)
-    should_parse("8", 8.0)
-    should_parse("8.25", 8.25)
+    should_parse_fraction("3/4", 0.75)
+    should_parse_fraction("2 1/16", 2.0625)
+    should_parse_fraction("8", 8.0)
+    should_parse_fraction("8.25", 8.25)
   end
 
   it "should otherwise just convert to a float" do
-    should_parse("2.0 3/4", 2.0)
-    should_parse("2 3 /4", 2.0)
+    should_parse_fraction("2.0 3/4", 2.0)
+    should_parse_fraction("2 3 /4", 2.0)
   end
 
 end
